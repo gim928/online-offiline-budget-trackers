d94f95c9d9933e8d3c4f0a0faa5ae2bccf87f245
Gina Im <eaim928@gmail.com>
2021-09-24 18:22:57 -0600

    added initial functions to service worker

---
+++ public/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "index.html",
+  "/manifest.webmanifest",
+  "public/assets/css/style.css",
+  "public/assets/js/index.js",
+  "public/assets/icons/icon-192x192.png",
+  "public/assets/icons/icon-512x512.png",
+];
+
+//open a cache
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("your files were precached sucessfully");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+//manage old caches after activating the service worker
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
