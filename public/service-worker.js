03d3b7885330ff6350664e07853fd2cd0efc572f
Gina Im <eaim928@gmail.com>
2021-09-29 08:19:20 -0600

    deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,25 +41,30 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-//event listener for fetch
-self.addEventListener("fetch", (evt) => {
+self.addEventListener("fetch", function (evt) {
+  // cache successful requests to the API
   if (evt.request.url.includes("/api/")) {
-    console.log("[Service Worker] Fetch(data)", evt.request.url);
-
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-          .then((response) => {
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch((err) => {
-            return cache.match(evt.request);
-          });
-      })
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
+
     return;
   }
 
