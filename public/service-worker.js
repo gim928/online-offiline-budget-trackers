fd8d8b05ecd4e3812c3b279fa4530ab316af418f
Gina Im <eaim928@gmail.com>
2021-09-29 11:04:48 -0600

    uncommented

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,56 +23,56 @@ self.addEventListener("install", function (evt) {
   self.skipWaiting();
 });
 
-//manage old caches after activating the service worker
-// self.addEventListener("activate", function (evt) {
-//   evt.waitUntil(
-//     caches.keys().then((keyList) => {
-//       return Promise.all(
-//         keyList.map((key) => {
-//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-//             console.log("Removing old cache data", key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   );
+// manage old caches after activating the service worker
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 
-//   self.clients.claim();
-// });
+  self.clients.claim();
+});
 
-// self.addEventListener("fetch", function (evt) {
-//   // cache successful requests to the API
-//   if (evt.request.url.includes("/api/")) {
-//     evt.respondWith(
-//       caches
-//         .open(DATA_CACHE_NAME)
-//         .then((cache) => {
-//           return fetch(evt.request)
-//             .then((response) => {
-//               // If the response was good, clone it and store it in the cache.
-//               if (response.status === 200) {
-//                 cache.put(evt.request.url, response.clone());
-//               }
+self.addEventListener("fetch", function (evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
 
-//               return response;
-//             })
-//             .catch((err) => {
-//               // Network request failed, try to get it from the cache.
-//               return cache.match(evt.request);
-//             });
-//         })
-//         .catch((err) => console.log(err))
-//     );
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
 
-//     return;
-//   }
+    return;
+  }
 
-//   // if the request is not for the API, serve static assets using
-//   // "offline-first" approach.
-//   evt.respondWith(
-//     caches.match(evt.request).then((response) => {
-//       return response || fetch(evt.request);
-//     })
-//   );
-// });
+  // if the request is not for the API, serve static assets using
+  // "offline-first" approach.
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
